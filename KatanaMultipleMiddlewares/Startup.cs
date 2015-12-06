3aa4d238057e50614cc4e8f799387509d1270031
DaveAhearne <david.ahearnex@gmail.com>
2015-12-06 22:36:15 +0000

    Initial Commit

---
+++ KatanaMultipleMiddlewares/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Owin;
+
+namespace KatanaMultipleMiddlewares
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            // When having multiple middlewares, the ordering is essential, higher middlewares are executed first in the pipeline:
+
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+
+                /*
+                 * When you have more than one middleware in your pipeline, you have to execute the next middleware in the pipeline
+                 * via the delegate thats passed in, if this wasn't done the pipeline would die at the first request.
+                 */
+
+                await next(); // This signals the end of the forward pass through
+
+                /*
+                 * At this point we've already sent the response headers, and the response body is now coming back
+                 * we can no longer change the response headers, but we can still append to the response body or execute arbitrary code
+                 * (This behavior can be changed, but its not easy to do.)
+                 */
+
+                Debug.WriteLine("Outgoing request: " + ctx.Request.Path);
+            });
+
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("World");
+            });
+        }
+    }
+}
---
+++ OwinKatanaNancyExample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+
+namespace KatanaOwinSimple
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                // Katana API implementation
+                //await ctx.Response.WriteAsync("Hello World");
+
+                // Raw implementation
+                //var responseStream = (Stream)ctx.Environment["owin.ResponseBody"];
+                //const string outputString = "Hello World";
+                //await responseStream.WriteAsync(Encoding.UTF8.GetBytes(outputString), 0, outputString.Length);
+
+                // If you want HTML output, you have to write it yourself for the moment:
+                await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World</h1></body></html>");
+            });
+        }
+    }
+}
